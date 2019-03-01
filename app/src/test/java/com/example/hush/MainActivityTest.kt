a544dd2b647a0c279153c40fc79db54d97f5b735
Heather Minke <heather.a.minke@gmail.com>
2019-03-01 13:22:52 -0700

    Added MainActivityTest class and started a test for onCreate().

---
+++ app/src/test/java/com/example/hush/MainActivityTest.kt
@@ -0,0 +1,34 @@
+package com.example.hush
+
+
+import android.R
+import android.widget.Button
+import android.widget.TextView
+import com.example.hush.MainActivity
+import org.junit.runner.RunWith
+import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertThat
+import org.junit.Test
+import org.junit.Assert
+import org.robolectric.Robolectric
+import org.robolectric.RobolectricTestRunner
+
+//import android.R
+//import android.app.Activity
+//import android.app.Activity
+//import android.widget.TextView
+//import org.junit.runner.RunWith
+//import org.robolectric.Robolectric
+//import org.robolectric.RobolectricTestRunner
+
+@RunWith(RobolectricTestRunner::class)
+
+class MainActivityTest {
+
+    @Test
+    @Throws(Exception::class)
+    fun onCreate() {
+        
+    }
+
+}
