0af771b8c5b96062d50bba223b11eca121a9e061
DarkLORDCOSMOS <jsamson3@msudenver.edu>
2019-03-20 22:02:37 -0600

    Revert "Revert "Revert "Revert "Revert "Merge pull request #6 from DarkLORDCOSMOS/feature/initialUnitTests"""""
    This reverts commit 3f4f2da0df8654cef9e4de166826794073a969c4.

---
+++ app/src/test/java/com/example/hush/ExampleUnitTest.kt
@@ -0,0 +1,23 @@
+package com.example.hush
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see [Testing documentation](http://d.android.com/tools/testing)
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        System.out.println("Running Unit Tests!")
+        assertEquals(4, (2 + 2).toLong())
+    }
+
+    @Test
+    fun onCreate() {
+
+    }
+}
