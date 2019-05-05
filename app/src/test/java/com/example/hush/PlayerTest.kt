30c7ae63739ed4c7df1558326fc2642f2b85dcd8
DarkLORDCOSMOS <jsamson3@msudenver.edu>
2019-05-04 22:52:15 -0600

    Update PlayerTest.kt

--- app/src/test/java/com/example/hush/PlayerTest.kt
+++ app/src/test/java/com/example/hush/PlayerTest.kt
@@ -1,6 +1,7 @@
 package com.example.hush
 
 import android.media.MediaPlayer
+import android.os.Environment
 //import org.junit.Assert.assertThat
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
@@ -8,24 +9,43 @@ import java.io.File
 //import javax.sql.DataSource
 import org.junit.Before
 import org.junit.Test
+import java.io.IOException
 
 @RunWith(RobolectricTestRunner::class)
 
 class PlayerTest {
     lateinit var testPlayer: Player
-    lateinit var mediaPlayer: MediaPlayer
+    // lateinit var mediaPlayer: MediaPlayer
 
     @Before
-    fun setup(){
+    fun setup() {
         testPlayer = Player()
-        mediaPlayer = MediaPlayer()
+        // mediaPlayer = MediaPlayer()
        // mediaPlayer = Shadow.newInstanceOf(MediaPlayer::class.java)
         //shadowMediaPlayer = shadowOf(mediaPlayer)
     }
 
     @Test
     @Throws(Exception::class)
-    fun testPlay(){
+    fun testSetup() {
+        // val file: File? = null
+        if (::testPlayer.isInitialized) {
+            val path = File(Environment.getExternalStorageDirectory().getPath())
+            try {
+                val file = File.createTempFile("temporary", ".3gp", path)
+                if(file != null) {
+                    testPlayer.setup(file)
+                    testPlayer.play()
+                }
+            } catch (e: IOException) {
+            }
+        }
+    }
+
+
+    @Test
+    @Throws(Exception::class)
+    fun testPlay() {
         val file: File? = null
         if (::testPlayer.isInitialized) {
             if(file != null) {
@@ -40,13 +60,13 @@ class PlayerTest {
     fun testPrepareForPlayback() {
         val file: File? = null
         if (::testPlayer.isInitialized) {
-            if(file != null){
-            testPlayer.setup(file)
-            mediaPlayer.start()
-                if (::mediaPlayer.isInitialized) {
-                    mediaPlayer.prepare()
-                }
-            }
+            if(file != null) {
+                testPlayer.setup(file)
+//                mediaPlayer.start()
+//                    if (::mediaPlayer.isInitialized) {
+//                        mediaPlayer.prepare()
+//                    }
+               }
             }
         }
     }
