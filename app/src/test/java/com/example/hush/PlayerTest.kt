daf1d948f07310e03ca81b867eea55c05125ccb5
Nila <nniroula@msudenver.edu>
2019-05-06 05:42:45 -0600

    added one more unit test for Player class.

--- app/src/test/java/com/example/hush/PlayerTest.kt
+++ app/src/test/java/com/example/hush/PlayerTest.kt
@@ -1,16 +1,13 @@
 package com.example.hush
 
 import android.media.MediaPlayer
-//import org.junit.Assert.assertThat
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 import java.io.File
-//import javax.sql.DataSource
 import org.junit.Before
 import org.junit.Test
 
 @RunWith(RobolectricTestRunner::class)
-
 class PlayerTest {
     lateinit var testPlayer: Player
     lateinit var mediaPlayer: MediaPlayer
@@ -21,6 +18,35 @@ class PlayerTest {
         mediaPlayer = MediaPlayer()
        // mediaPlayer = Shadow.newInstanceOf(MediaPlayer::class.java)
         //shadowMediaPlayer = shadowOf(mediaPlayer)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun testSetupFileForPlayback() {
+        val file: File? = null
+        if (::testPlayer.isInitialized) {
+            if (file != null) {
+                testPlayer.setup(file)
+                if (::mediaPlayer.isInitialized) {
+                    mediaPlayer.setDataSource(file.absolutePath)
+                }
+            }
+        }
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun testPrepareForPlayback() {
+        val file: File? = null
+        if (::testPlayer.isInitialized) {
+            if(file != null){
+                testPlayer.setup(file)
+                mediaPlayer.start()
+                if (::mediaPlayer.isInitialized) {
+                    mediaPlayer.prepare()
+                }
+            }
+        }
     }
 
     @Test
@@ -37,33 +63,8 @@ class PlayerTest {
 
     @Test
     @Throws(Exception::class)
-    fun testPrepareForPlayback() {
-        val file: File? = null
-        if (::testPlayer.isInitialized) {
-            if(file != null){
-            testPlayer.setup(file)
-            mediaPlayer.start()
-                if (::mediaPlayer.isInitialized) {
-                    mediaPlayer.prepare()
-                }
-            }
-            }
+    fun shouldThrowNullPointerException(){
+        // implement an exception to test-try catch block.
         }
-
-    @Test
-    @Throws(Exception::class)
-    fun testSetupFileForPlayback() {  // this is a private method
-        val file: File? = null
-        //testPlayer.setup(file)
-        if (::testPlayer.isInitialized) {
-            if (file != null) {
-                testPlayer.setup(file)
-                if (::mediaPlayer.isInitialized) {
-                    mediaPlayer.setDataSource(file.absolutePath)
-                }
-            }
-        }
-    }
-
 }
 
